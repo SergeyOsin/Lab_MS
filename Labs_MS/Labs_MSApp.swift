834c189b38d946bb4875b6ea6d7ffc08cf75480b
Sergey Osin <sereosi2017@yandex.ru>
2026-03-10 15:58:46 +0300

    Initial Commit

---
+++ Labs_MS/Labs_MSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Labs_MSApp.swift
+//  Labs_MS
+//
+//  Created by Sergey Osin on 10.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Labs_MSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
